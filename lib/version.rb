d570a9adc7d8fe51d298bcb75fd18c4158cb6080
Pixelastic <tim@pixelastic.com>
2018-03-20 13:59:42 +0100

    release 2.5.2

--- lib/version.rb
+++ lib/version.rb
@@ -1,6 +1,6 @@
 # Expose gem version
 # rubocop:disable Style/SingleLineMethods
 class AlgoliaHTMLExtractorVersion
-  def self.to_s; '2.5.1' end
+  def self.to_s; '2.5.2' end
 end
 # rubocop:enable Style/SingleLineMethods
